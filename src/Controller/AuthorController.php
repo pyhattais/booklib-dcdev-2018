5403d1e4657d9abfa39db026b83428b493f72b59
PierreYvesHattais <py.hattais@gmail.com>
2018-11-23 10:58:36 +0100

    CRUD des box

--- src/Controller/AuthorController.php
+++ src/Controller/AuthorController.php
@@ -1,15 +1,44 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: PYHattais
- * Date: 22/11/2018
- * Time: 16:08
- */
 
 namespace App\Controller;
 
 
-class AuthorController
+use App\Entity\Author;
+use App\Form\AuthorType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class AuthorController
+ * @package App\Controller
+ * @Route("/author")
+ */
+class AuthorController extends BaseController
 {
+
+    /**
+     * @Route("/new", name="author_new")
+     */
+    public function new(Request $request)
+    {
+        $author = new Author();
+
+        $form = $this->createForm(AuthorType::class, $author);
+
+        $form->handleRequest($request);
+
+        if (($form->isSubmitted() && $form->isValid())) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($author);
+            $em->flush();
+
+            return $this->redirectToRoute('homepage');
+        }
+
+
+        return $this->render('author/new.html.twig', ['form' => $form->createView()]);
+    }
 
 }
