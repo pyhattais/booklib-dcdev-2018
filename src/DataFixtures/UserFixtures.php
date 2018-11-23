693011d817cdb3fcb5eb72d1636a59b7c0c6a5ec
PierreYvesHattais <py.hattais@gmail.com>
2018-11-23 15:57:37 +0100

    Gestion des utilisateurs

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -6,14 +6,39 @@ namespace App\DataFixtures;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 use App\Entity\User;
+use Faker\Factory;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class UserFixtures extends Fixture
 {
 
+    private $passwordEncoder;
+
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
     public function load(ObjectManager $manager)
     {
+        $admin = new User();
+        $admin->setFirstname("Pierre-Yves");
+        $admin->setLastname("Hattais");
+        $admin->setEmail("py.hattais@gmail.com");
+        $admin->setPassword($this->passwordEncoder->encodePassword($admin, "pyhattais"));
+        $admin->setRoles(["ROLE_ADMIN"]);
+        $manager->persist($admin);
+
+        $faker = Factory::create('fr_FR');
+
         for ($i = 0; $i < 20; $i++) {
             $user = new User();
+            $user->setFirstname($faker->firstName);
+            $user->setLastname($faker->lastName);
+            $user->setEmail($faker->email);
+            $user->setPassword($this->passwordEncoder->encodePassword($user, "1234"));
+            $user->setRoles(["ROLE_USER"]);
             $manager->persist($user);
             $this->addReference('user-' . ($i + 1), $user);
         }
